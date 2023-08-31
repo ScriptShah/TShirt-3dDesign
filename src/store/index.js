4fa41d56fe07f605bf5c1957c107c80e3fd29c8d
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2023-08-31 12:54:34 +0430

    change the default colo

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { proxy } from "valtio";
 
 const state = proxy ({
     intro : true,
-    color: '#EFBD48',
+    color: '#2ccce4',
     isLogoTexture: false,
     isFullTexture: false,
     logoDecal : './threejs.png',
